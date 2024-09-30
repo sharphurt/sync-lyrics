cda8daeb32f720aa86479245c73aabf250957511
Лукоянов Павел <Pavel.Lukoianov@urfu.me>
2024-09-30 20:00:15 +0500

    Initial commit

---
+++ src/main/java/ru/sharphurt/synclyrics/SynclyricsApplication.java
@@ -0,0 +1,13 @@
+package ru.sharphurt.synclyrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynclyricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynclyricsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sharphurt/synclyrics/SynclyricsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sharphurt.synclyrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynclyricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
